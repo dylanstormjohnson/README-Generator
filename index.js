f4173cd4d420ce7a058785c7f5a8dbffd1dc3078
Dylan Storm Johnson <dylanstormjohnson@comcast.net>
2023-04-27 17:47:11 -0500

    Final Draft

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 const inquirer = require("inquirer");
 
+let licenseInfo = {}
+
 const init = async () => {
   try {
     const data = await inquirer.prompt([
@@ -49,6 +51,11 @@ const init = async () => {
         type: "list",
         message: "What file type is said image?",
         choices: ["jpeg", "png", "pdf"],
+      },
+      {
+        name: "imageDescription",
+        type: "input",
+        message: "Give a short description of the image.",
       },
       {
         name: "motivation",
@@ -105,11 +112,37 @@ const init = async () => {
         type: "input",
         message: "List a feature this project includes. (5/5)",
       },
+      {
+        name: "contributionGuidelines",
+        type: "input",
+        message: "Explain how others can contribute to your project.",
+      },
+      {
+        name: "license",
+        type: "list",
+        message: "Which license is this project under?",
+        choices: ["Apache License 2.0 (Communal work)", "MIT (Simple, permissive)", "GNU General Public License v3.0 (Improvement sharing)"],
+      },
+      {
+        name: "test1Instructions",
+        type: "input",
+        message: "List instructions on how conduct the first test on your project.",
+      },
+      {
+        name: "test2Instructions",
+        type: "input",
+        message: "List instructions on how conduct the second test on your project.",
+      },
+      {
+        name: "test3Instructions",
+        type: "input",
+        message: "List instructions on how conduct the third test on your project.",
+      },
     ]);
 
     // console.log(data);
     console.log(
-      "Don't forget to edit the README to add more features, credits, a license, and more !!!!!!!!"
+      "Don't forget to edit the README to add more features, credits, badges, and more !!!!!!!!"
     );
 
     const {
@@ -120,6 +153,7 @@ const init = async () => {
       languages,
       imageName,
       imageFileType,
+      imageDescription,
       motivation,
       why,
       problemSolved,
@@ -131,11 +165,44 @@ const init = async () => {
       featureThree,
       featureFour,
       featureFive,
+      contributionGuidelines,
+      license,
+      test1Instructions,
+      test2Instructions,
+      test3Instructions,
     } = data;
+
+    // if (license === "Apache License 2.0 (Communal work)") {
+
+    // } else if if (license === "Apache License 2.0 (Communal work)")
+
+    switch (license) {
+      case "Apache License 2.0 (Communal work)":
+        var licenseInfo = {
+          title: "Apache License 2.0",
+          description: "use must still preserve copyright and license notices.  Distribution under separate terms is allowed for licensed works, modifications, and larger works.  An express grant of patent rights is provided by contributors.",
+          badge: "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+        };
+        break;
+      case "MIT (Simple, permissive)":
+        var licenseInfo = {
+          title: "MIT License",
+          description: "use must still preserve copyright and license notices.  Distribution under separate terms is allowed for licensed works, modifications, and larger works.",
+          badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+        };
+        break;
+      case "GNU General Public License v3.0 (Improvement sharing)":
+        var licenseInfo = {
+          title: "GNU General Public License v3.0",
+          description: "complete source code of licensed work and modifications must be made available under the same license.  Use must also preserve copyright and license notices.  An express grant of patent rights is provided by contributors.",
+          badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+        };
+        break;
+    }
 
     const readMe = () => `
 # <${title}>
-
+${licenseInfo.badge}
 ## Description
 
 - This project was made using ${languages.join(", ")}.  I was motivated to build this project ${motivation}  It was built because ${why}  It is intended to solve the problem of ${problemSolved}  In making said project, I learned ${learned}
@@ -156,7 +223,27 @@ const init = async () => {
 -${usage}
 
 - Here is an image of the completed project:
-![Description of the project's image...](assets/images/${imageName}.${imageFileType})
+![${imageDescription}](assets/images/${imageName}.${imageFileType})
+
+
+## Credits
+-This website was coded by ${firstName} ${middleName} ${surname}.
+
+-List your collaborators, if any, with links to their GitHub profiles.
+
+-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+-If you followed tutorials, include links to those here as well.
+
+## License
+
+-This project is listed under the ${licenseInfo.title}.  Under this license, ${licenseInfo.description}
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
 ## Features
 
@@ -170,19 +257,19 @@ const init = async () => {
 
 -${featureFive}
 
-## Credits
--This website was coded by ${firstName} ${middleName} ${surname}.
+## How to Contribute
 
--List your collaborators, if any, with links to their GitHub profiles.
+-${contributionGuidelines}
 
--If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+-Aside from the aforementioned ways you can contribute, the [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard for contribution policy.  (Please use the above guidelines for contribution and consult the Contributor Covenant on things not specified.)
 
--If you followed tutorials, include links to those here as well.
+## Tests
 
-## License
+-${test1Instructions}
 
--If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+-${test2Instructions}
 
+-${test3Instructions}
 ---`;
 
     await fs.promises.writeFile("README.md", readMe());
